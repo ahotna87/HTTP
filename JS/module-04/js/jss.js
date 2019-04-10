5b9c1b879bea2d719310a5b18a8c2eebf2beb4d4
ahotna <ahotna87@gmail.com>
2019-04-10 19:05:47 +0300

    module 04

--- JS/module-04/js/jss.js
+++ JS/module-04/js/jss.js
@@ -84,6 +84,7 @@
 //   Далее выполнение идет по вышеописанному алгоритму.
 // */
 
+
 const products = {
   bread: 10,
   milk: 15,
@@ -95,38 +96,31 @@ const products = {
   candy: 25
 };
 
-
-
-function Cashier(name) {
-  this.name = name;
-}
-
-
-function Cashier({ name, products, totalPrice = 0, customerMoney = 0, changeAmount = 0 }) {
+function Cashier(name, products) {
   this.name = name;  //строка, имя, передается при вызове конструктора
   this.products = products; //объект база данных продуктов, передается при вызове конструктора
-  this.totalPrice = totalPrice; //число, общая стоимость покупок текущего покупателя, всегда начинается с 0
-  this.customerMoney = customerMoney; //число, сумма введенная пользователем в prompt при запросе денег, всегда начинается с 0
-  this.changeAmount = changeAmount; //число, сдача, всегда начинается с 0
+  this.totalPrice = 0; //число, общая стоимость покупок текущего покупателя, всегда начинается с 0
+  this.customerMoney = 0; //число, сумма введенная пользователем в prompt при запросе денег, всегда начинается с 0
+  this.changeAmount = 0; //число, сдача, всегда начинается с 0
 
   this.countTotalPrice = function (order) {
     for (let item in order) {
       this.totalPrice += order[item] * this.products[item];
-    }
-    return this.totalPrice;
+    } return this.totalPrice;
   } //метод, получает список покупок, считает общую сумму исходя из поля products
 
-  this.getCustomerMoney = function (value) {
-    this.customerMoney = value;
-
+  this.getCustomerMoney = function () {
+    this.customerMoney = prompt(`Сумма Ваших покупок составляет ${this.totalPrice} \n Укажите сумму, которую дал клиент`);
   } //метод, при вызове показывает prompt, в котором указана общая сумма покупок из поля totalPrice.
 
   this.countChange = function () {
-    if (this.customerMoney < this.totalPrice) {
-      return false;
+    if (this.totalPrice <= this.customerMoney) {
+      this.changeAmount = this.customerMoney - this.totalPrice;
+      return alert(`Спасибо за покупку, ваша сдача ${this.changeAmount}`);
     }
-    this.changeAmount = this.customerMoney - this.totalPrice;
-    return this.changeAmount;
+
+    return alert(`Очень жаль, что-то пошло не так, приходите еще.`);
+
   } //метод, считает сдачу, разницу между общей суммой покупок и деньгами покупателя.
 
   this.reset = function () {
@@ -136,7 +130,13 @@ function Cashier({ name, products, totalPrice = 0, customerMoney = 0, changeAmou
   }// метод, сбрасывает поля totalPrice, customerMoney и changeAmount в 0.
 
   this.serve = function (order) {
+    console.log(`Здравствуйте, Вас обслуживает кассир `, this.name);
 
+    this.countTotalPrice(order);
+    this.getCustomerMoney();
+    this.countChange();
+    this.reset();
+    console.log(`totalPrice = ${this.totalPrice}, customerMoney = ${this.customerMoney}, changeAmount = ${this.changeAmount}`);
 
   }  //метод, вызывается при обслуживании пользователя. 
   //Получает order - список покупок пользователя, вызывает метод countTotalPrice для подсчета общей суммы, 
@@ -146,51 +146,22 @@ function Cashier({ name, products, totalPrice = 0, customerMoney = 0, changeAmou
   //Вызывает метод reset при любом исходе обслуживания.
 }
 
-const cashier = new Cashier('Mango', products);
-console.log(cashier);
-cashier.getCustomerMoney(120);
 
+/* Заказ пользователя хранится в виде объекта следующего формата. "имя-продукта":"количество-единиц" */
+const order1 = { bread: 2, milk: 2, apples: 1, cheese: 1 };
+const order2 = { bread: 1, milk: 1, apples: 1, chicken: 2, cheese: 2, tea: 2 };
+const order3 = { bread: 3, apples: 5, cheese: 1, candy: 4 };
+const order4 = { milk: 3, apples: 4, chicken: 1, candy: 6, cheese: 3 };
+const order5 = { bread: 2, milk: 2, cheese: 1, tea: 5 };
+/* Создадим кассиров */
+const vasya = new Cashier("Vasya", products);
+const anna = new Cashier("Anna", products);
+const mango = new Cashier('Mango', products);
 
+console.log(`Список товаров и цены нашего магазина`, products);
 
-
-
-
-
-
-
-
-
-function Cashier(foods) {
-  this.foods = foods;
-  this.money = 0;
-  this.sum = 0;
-
-  this.totalPrice = function (cart) {
-
-    for (let i in cart) {
-      for (let j in this.foods) {
-        if (cart[i] === j) this.sum = this.sum + this.foods[j];
-      }
-    }
-    alert(`goods, ${this.sum}`);
-  }
-
-  this.getMoney = function () {
-    this.money = prompt('dsfsdfsdf')
-  }
-
-  this.change = function () {
-    console.log(this.money - this.sum);
-  }
-}
-
-const vasya = new Cashier({ qwe: 10, rty: 20, vbnm: 30, dfg: 30, wer: 50 });
-
-
-const cart = ['qwe', 'rty', 'dfg'];
-vasya.totalPrice(cart);
-
-
-vasya.getMoney();
-
-vasya.change();
+vasya.serve(order1);
+anna.serve(order2);
+vasya.serve(order3);
+anna.serve(order4);
+mango.serve(order5);
