42553028f2bd460ff16fce5eb08a32bfc47d1cd3
ahotna <ahotna87@gmail.com>
2020-04-26 20:04:38 +0300

    Modile 07

--- JS/module-07/js/script.js
+++ JS/module-07/js/script.js
@@ -1,161 +1,84 @@
 `use strict`;
 
-// let numberA = +prompt("Введите первое число ", ``);
-// let numberB = +prompt("Введите второе число ", ``);
-// let sumOfNumbers = 0;
+const posts = [
+  {
+    img: "https://placeimg.com/400/150/people",
+    title: "Post title 1",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-1.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/nature",
+    title: "Post title 2",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-2.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/animals",
+    title: "Post title 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-3.com'
+  },
+  {
+    img: "https://placeimg.com/400/150/tech",
+    title: "Post title 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-3.com'
+  },{
+    img: "https://placeimg.com/400/150/architecture",
+    title: "Post title 3",
+    text: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga, nemo dignissimos ea temporibus voluptatem maiores maxime consequatur impedit nobis sunt similique voluptas accusamus consequuntur, qui modi nesciunt veritatis distinctio rem!",
+    link: 'link-3.com'
+  }
+];
 
-// const isNumeric = function(a, b) {
-//   return !isNaN(parseFloat(a, b)) && isFinite(a, b);
+const grid = document.querySelector('.card__grid');
+const elements = cteatePostCard(posts);
+
+console.log(elements);
+
+grid.append(...elements);
+
+function cteatePostCard(posts){
+  return posts.reduce((acc,el) => acc.concat(createCards(el)),[]);
+};
+
+// function cteateCardsList(arr){
+//   const elements =[];
+
+// arr.forEach(post => {
+//   const el = createCards(post);
+//   elements.push(el);
+// } )
+// return elements;
 // };
-// if (isNumeric(numberA, numberB)) {
-//   sumOfNumbers = numberA + numberB;
-//   alert(`Сумма чисел ${numberA} + ${numberB} равна:\n ${sumOfNumbers}`);
-// } else alert("Ошибочка!");
 
-// let numberA = +prompt("Введите первое число ", ``);
-// let numberB = +prompt("Введите второе число ", ``);
-// let sumOfNumbers = 0;
+function createCards({img, title, text, link}) {
 
-// const isNumeric = function(a, b) {
-//   return !isNaN(parseFloat(a, b)) && isFinite(a, b);
-// };
-// if (isNumeric(numberA, numberB)) {
-//   sumOfNumbers = numberA + numberB;
+const post__image = document.createElement ('img');
+post__image.classList.add ('post__image');
+post__image.src = img;
+post__image.alt = 'post__image';
 
-//   alert(
-//     `Сумма чисел ${numberA} + ${numberB} равна:\n ${sumOfNumbers.toFixed(2)}`
-//   );
-// } else alert("Ошибочка!");
+const post = document.createElement('div');
+post.classList.add('post');
 
-// let i = 0;
-// while (i < 11) {
-//   i += 0.2;
-//   alert(i.toFixed(1));
-// }
+const post__title = document.createElement('h2');
+post__title.textContent = title;
+post__title.classList.add('post__title');
 
-// function fib(n) {
-//   let a = 1,
-//     b = 0,
-//     x;
-//   for (i = 0; i < n; i++) {
-//     x = a + b;
-//     a = b;
-//     b = x;
-//   }
-//   return b;
-// }
-// alert(fib(4));
+const post__description = document.createElement('p');
+post__description.classList.add('post__description');
+post__description.textContent = text;
 
-// function fibBinet(n) {
-//   let a = (1 + Math.sqrt(5)) / 2;
-//   let fibonachi = Math.pow(a, n) / Math.sqrt(5);
-//   return fibonachi;
-// }
+const post__link = document.createElement('a');
+post__link.classList.add('post__link');
+post__link.href = link;
+post__link.textContent = 'Ссылка';
 
-// alert(fibBinet(4));
+post.append(post__image, post__title, post__description, post__link);
 
-// let numberA = +prompt("Введите начальное число диапазона", ``);
-// let numberB = +prompt("Введите конечное число диапазона ", ``);
-// let x = numberA + Math.random() * (numberB - numberA);
-// console.log(x);
+return post;
 
-// function checkSpam(str) {
-//   let lower = str.toLowerCase();
-//   return alert(!!(~lower.indexOf("viagra") || ~lower.indexOf("xxx")));
-// }
+}
 
-// function extractCurrencyValue(str) {
-//   return +str.slice(1);
-// }
-// alert(extractCurrencyValue("$120"));
-
-// function isEmpty(obj) {
-//   for (var code in schedule) {
-//     return true;
-//   }
-//   return false;
-// }
-// let schedule = {};
-
-// alert(isEmpty(schedule)); // true
-
-// schedule["8:30"] = "подъём";
-
-// alert(isEmpty(schedule)); // false
-
-// let fruits = ["Яблоко", "Груша", "Слива"];
-
-// fruits.push("Компютер");
-// let lastItem = fruits[fruits.length - 1];
-// fruits[fruits.length] = "Компютер";
-// alert(lastItem);
-
-// let style = [`Джаз`, ` Блюз`];
-// alert(style);
-
-// style.push(` Рок-н-Ролл`);
-// alert(style);
-
-// style[style.length - 2] = ` Классика`;
-// alert(style);
-
-// style.shift(0);
-// alert(style);
-
-// style.unshift(` Рэп`, ` Рэгги`);
-// alert(style);
-
-// let arr = ["Яблоко", "Апельсин", "Груша", "Лимон", "Персик", "Абрикос"];
-
-// let rand = Math.floor(Math.random() * arr.length);
-
-// alert(arr[rand]);
-
-// let numbers = [];
-
-// while (true) {
-//   let value = prompt("Введите число", 0);
-
-//   if (value === "" || value === null || isNaN(value)) break;
-
-//   numbers.push(+value);
-// }
-
-// let sum = 0;
-// for (let i = 0; i < numbers.length; i++) {
-//   sum += numbers[i];
-// }
-
-// alert(sum);
-
-// let filterRange = function (arr, a, b){
-//   let result = [];
-//   for (let i=0; i < arr.length; i++){
-//   if (arr[i]>=a && arr[i]<=b){
-//     result.push(arr[i]);
-//       }
-//   }
-//   return result;
-// }
-// let arr = [5, 4, 3, 8, 0];
-// let filtered = filterRange(arr, 3, 5);
-// alert( filtered );
-
-// let arr = new Array(101);
-// for (let i = 0, j = 0; i <= arr.length - 1 && j <= arr.length; i++, j++) {
-//   arr[i] = j;
-// }
-// alert(arr);
-
-// let arr = [1, -2, 15, 2, 0, 8];
-// let rev = function(arr) {
-//   return arr.reverse();
-// };
-// console.log(rev(arr));
-
-var arr = ["HTML", "JavaScript", "CSS"];
-
-let arrSorted = arr.slice(`, `).sort();
-
-alert(arrSorted); // CSS, HTML, JavaScript
-alert(arr); // HTML, JavaScript, CSS (без изменений)
